f0f4292b715783291bb624dbe38b7f71253ca467
WeazelScreeps <tattedweazel@live.com>
2016-07-18 00:07:20 -0500

    updating upgrader scavenge state logic

--- role.upgrader.js
+++ role.upgrader.js
@@ -38,7 +38,13 @@ var roleUpgrader = {
     				HiveMind.setTarget(creep, target);
     			}
     		}
-
+    	} else if (target = HiveMind.canScavenge(creep)) {
+				if (creep.carry.energy < creep.carryCapacity) {
+					HiveMind.updateState(creep, 'scavenging');
+					HiveMind.setTarget(creep, target);
+				} else {
+					HiveMind.updateState(creep, 'upgrading');
+				}
     	} else {
 			if (creep.carry.energy > 0) {
 				HiveMind.updateState(creep, 'upgrading');
