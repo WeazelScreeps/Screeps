826caf64b9702c5eb523ec0ee011e8e329eb30df
WeazelScreeps <tattedweazel@live.com>
2016-07-17 22:11:03 -0500

    Ok seriously...

--- tower.handler.js
+++ tower.handler.js
@@ -2,22 +2,19 @@ var towerHandler = {
 
 	run: function() {
 
-		var towers = [
-			Game.getObjectById('578c3de0e949c1f60f168221')
-		];
+		var tower = Game.getObjectById('578c3de0e949c1f60f168221');
+		
+		var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
 
-		for(var tower in towers){
-			var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-	        if(closestHostile) {
-	            tower.attack(closestHostile);
-	        }
-	        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-	            filter: (structure) => structure.hits < structure.hitsMax
-	        });
-	        if(closestDamagedStructure) {
-	            tower.repair(closestDamagedStructure);
-	        }
-	    }
 	}	
 
 }
