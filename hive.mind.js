af7db9bf6c9d2e51a384ae13c8546e417cd1da79
WeazelScreeps <tattedweazel@live.com>
2016-07-19 21:15:16 -0500

    only resupply tower

--- hive.mind.js
+++ hive.mind.js
@@ -124,7 +124,8 @@ var hiveMind = {
 	canDeliver: function(creep) {
 		var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
+                	//return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
+                    return (structure.structureType == STRUCTURE_TOWER) &&
                         structure.energy < structure.energyCapacity;
                 }
         });
