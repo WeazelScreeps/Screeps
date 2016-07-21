33fee50d67f9aa1feee72cdfe058db840bd32b55
WeazelScreeps <tattedweazel@live.com>
2016-07-21 10:11:25 -0500

    Updating Storage logic

--- hive.mind.js
+++ hive.mind.js
@@ -147,7 +147,7 @@ var hiveMind = {
         var storage = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                 	return (structure.structureType == STRUCTURE_STORAGE) &&
-                        structure.energy < structure.energyCapacity;
+                        structure.store[RESOURCE_ENERGY] < structure.storeCapacity;
                 }
         });
         
